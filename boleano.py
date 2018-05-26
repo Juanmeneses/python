b2a1a244560851b590bacb83e31ea2858bf12235
Juan meneses <Juan.menesest@gmail.com>
2018-05-26 14:18:24 -0400

    segundo commit

---
+++ boleano.py
@@ -0,0 +1,10 @@
+import os, sys
+a= 1
+b = 2
+mensaje = "su valor es igual al del sistema"
+
+respuesta = (input('Introduzca un numero ')) 
+
+if(respuesta == a):
+    print 'ingresó', ', a, ",",mensaje
+       
