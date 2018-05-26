812f4110d1e7e356554b8afd480338d1c9d14457
Juan meneses <Juan.menesest@gmail.com>
2018-05-26 12:45:02 -0400

    primercommit

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+#sumatoria de cuatro numeros pedidos por pantalla, ademas imprimir una cadena con el resultado de los 4 numeros
+
+import os, sys
+
+a = (input ("primer numero: " ))
+b = (input ("segundo numero: " ))
+c = (input ("tercer numero: " ))
+d = (input ("cuarto numero: " ))
+
+mensaje = "La sumatoria de los numeros es: "
+resultado = a+b+c+d    #para no tener que calcular en el print
+
+print(mensaje, resultado)
